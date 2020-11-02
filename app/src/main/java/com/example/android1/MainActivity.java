abc5b992528fc1d20f06a7fa579e01b0aa23444f
yingjie <yingjie_qiao@outlook.com>
2020-11-02 19:43:15 +0800

    initial commit

---
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showStats(View view) {
+    }
+
+    public void showCook(View view) {
+    }
+
+    public void showSettings(View view) {
+    }
+
+    public void showCashier(View view) {
+    }
+}
