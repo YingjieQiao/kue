f5632ce64b746177f9a69247ffbc5576ce36510d
yingjie <yingjie_qiao@outlook.com>
2020-11-02 20:54:07 +0800

    basepage

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.android1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +19,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showStats(View view) {
+        Log.d(LOG_TAG, "Stats Button clicked!");
+        Intent intent = new Intent(this, StatsActivity.class);
+        startActivity(intent);
     }
 
     public void showCook(View view) {
+        Log.d(LOG_TAG, "Cook Button clicked!");
+        Intent intent = new Intent(this, CookActivity.class);
+        startActivity(intent);
     }
 
     public void showSettings(View view) {
     }
 
     public void showCashier(View view) {
+        Log.d(LOG_TAG, "Cashier Button clicked!");
+        Intent intent = new Intent(this, CashierActivity.class);
+        startActivity(intent);
     }
 }
