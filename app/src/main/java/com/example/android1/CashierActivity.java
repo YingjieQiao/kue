c06687ac6c00d10b49271a20603ffaa1ecb4a74e
yingjie <yingjie_qiao@outlook.com>
2020-11-16 15:41:35 +0800

    feat: push data sample

--- app/src/main/java/com/example/android1/CashierActivity.java
+++ app/src/main/java/com/example/android1/CashierActivity.java
@@ -1,14 +1,56 @@
 package com.example.android1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CashierActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            HomePageActivity.class.getSimpleName();
+    private String sharedPrefFile = "com.example.android1.mainsharedprefs";
+    SharedPreferences mPreferences;
+    public static String USER; // the current user logged in the app
+    public static String DB_KEY_USERNAME; // the child in firebase to query from
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cashier);
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        Button cash = findViewById(R.id.button_cash);
+
+        cash.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "foodfood Button clicked!");
+
+                String db_key_username = mPreferences.getString(DB_KEY_USERNAME, "ERROR");
+                DatabaseReference restaurant_db = database.getReference("accounts")
+                        .child(db_key_username);
+                HashMap<String, Object> hmap = new HashMap<>();
+                hmap.put("Food", "pasta");
+                hmap.put("OrderTime", System.currentTimeMillis());
+                restaurant_db.child("orders").push().setValue(hmap);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/android1/Utils.java
@@ -0,0 +1,30 @@
+package com.example.android1;
+
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
+public class Utils {
+    private static String sharedPrefFile = "com.example.android1.mainsharedprefs";
+    private static String USER; // the current user logged in the app
+    private static String DB_KEY_USERNAME; // the child in firebase to query from
+
+    public static void generate_db_key(SharedPreferences mPreferences) {
+        /*
+        This method is called when logged in. It takes in the logged in username and
+        generates the database key in firebase.
+        eg: user1@xxx --> username1; user2@xxx --> username2
+        The key is saved in shared preferences.
+         */
+
+        String username = mPreferences.getString(USER, "ERROR");
+        String[] username_split = username.split("@");
+        String username_before_domain = username_split[0];
+        Character num_ch = username_before_domain.charAt(username_before_domain.length()-1);
+        String db_key = "username" + num_ch;
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putString(DB_KEY_USERNAME, db_key);
+        preferencesEditor.apply();
+    }
+}
