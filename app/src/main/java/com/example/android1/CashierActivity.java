308b6cd6d019a2be72d5e13b2ba5737f127cfdaa
ryan <legacynightwalker@hotmail.sg>
2020-11-16 16:51:53 +0800

    created class order java which this will be pushed into data base , minor changes to counter not going lower than 0

--- app/src/main/java/com/example/android1/CashierActivity.java
+++ app/src/main/java/com/example/android1/CashierActivity.java
@@ -1,13 +1,20 @@
 package com.example.android1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class CashierActivity extends AppCompatActivity {
     int count=0;
@@ -17,91 +24,103 @@ public class CashierActivity extends AppCompatActivity {
     double pricing_duck_rice =4.50;
     double pricing_satay=3.50;
     double total_cost=0;
+    int numberoforders=0;
+    public FirebaseDatabase db = FirebaseDatabase.getInstance();//creating the database instance
+    private static final String TAG = CashierActivity.class.getSimpleName();
+    private String sharedPrefFile = "com.example.android1.mainsharedprefs";
+    SharedPreferences mPreferences;
+    public static String USER; // the current user logged in the app
+    public static String DB_KEY_USERNAME; // the child in firebase to query from
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView chickenricenumber = findViewById(R.id.integer_number);
-        TextView duckricenumber =findViewById(R.id.integer_number_2);
-        TextView sataynumber=findViewById(R.id.integer_number_3);
+        TextView duckricenumber = findViewById(R.id.integer_number_2);
+        TextView sataynumber = findViewById(R.id.integer_number_3);
+        TextView totalcost = findViewById(R.id.totalcostvalue);
         Button submitbutton = findViewById(R.id.submitbutton);
         submitbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-/*        Intent intent = new Intent(CashierActivity.this,StatsActivity.class);
-        intent.putExtra("Chickenrice",chickenricenumber + "Chicken Rice");
-        intent.putExtra("duckricenumber",duckricenumber+" Duck Rice");
-        intent.putExtra("satayricenumber",sataynumber + " Satay");
-        intent.putExtra("Cost",total_cost);
-        startActivity(intent);*/
-        if(chickenricenumber.getText().toString().equals("0") && duckricenumber.getText().toString().equals("0")&&sataynumber.getText().toString().equals("0")){
-            Toast.makeText(CashierActivity.this,"no Order",Toast.LENGTH_SHORT).show();
-        }else{
-        chickenricenumber.setText("0");
-        duckricenumber.setText("0");
-        sataynumber.setText("0");
-        Toast.makeText(CashierActivity.this,"Order Submitted",Toast.LENGTH_SHORT).show();
-
-            }}
+                DatabaseReference restaurant_db = db.getReference("accounts");
+                Orders order=new Orders("Order " + numberoforders,chickenricenumber.getText().toString(),duckricenumber.getText().toString(),sataynumber.getText().toString(),totalcost.getText().toString());
+                restaurant_db.child("orders").push().setValue(order);
+            }
         });
     }
 
-    public void increaseInteger(View v) {
+
+        public void increaseInteger(View v) {//increase the number of counter of counter of chicken rice
         count = count + 1;
         total_cost=total_cost+pricing_chicken_rice;//writing the price of the food
         display(count);
         display_4(total_cost);
 
-    }public void decreaseInteger(View v){
+    }public void decreaseInteger(View v){// the number of counter for chicken rice
+        TextView chickenrice =findViewById(R.id.integer_number);
+        if(Integer.parseInt(chickenrice.getText().toString())>0){//checks whether the counter value is >=0 so that normal additionals can be done
         count = count - 1;
         total_cost=total_cost-pricing_chicken_rice;//writing the price of the food
         display_4(total_cost);
-        display(count);
+        display(count);}
+        else{//this checks whether is it = 0 anot
+            display_4(0);//this is to prevent the value of the counter to go below 0 when pressed
+
+        }
     }
 
-    private void display(int number) {
+    private void display(int number) {//display value for the chicken rice counter
         TextView displayInteger = (TextView) findViewById(
                 R.id.integer_number);
         displayInteger.setText("" + number);
     }
     //this is for second button onwards
-    public void increaseInteger_2(View v) {
+    public void increaseInteger_2(View v) {//increase the counter for the duck rice
         count_2 = count_2 + 1;
         total_cost=total_cost+pricing_duck_rice;//writing the price of the food
         display_4(total_cost);
 
         display_2(count_2);
 
-    }public void decreaseInteger_2(View v){
+    }public void decreaseInteger_2(View v){//drecrease the counter for the duck rice
+        TextView duckrice = findViewById(R.id.integer_number_2);//find id of the duck rice
+        if(Integer.parseInt(duckrice.getText().toString())>0){
         count_2 = count_2 - 1;
         total_cost=total_cost-pricing_duck_rice;//writing the price of the food
         display_4(total_cost);
-        display_2(count_2);
+        display_2(count_2);}
+        else{
+            display_2(0);//set this counter to 0 if try to decrease below 0
+        }
 
 
     }
 
-    private void display_2(int number) {
+    private void display_2(int number) {//display for the duck rice
         TextView displayInteger = (TextView) findViewById(
                 R.id.integer_number_2);
         displayInteger.setText("" + number);
     }
     //this is for third button onwards
-    public void increaseInteger_3(View v) {
+    public void increaseInteger_3(View v) {//increase counter for the satay
         count_3 = count_3 + 1;
         total_cost=total_cost+pricing_satay;//writing the price of the food
         display_4(total_cost);//pricing of food
         display_3(count_3);}
 
 
-    public void decreaseInteger_3(View v){
+    public void decreaseInteger_3(View v){//decrease for the counter for satay
+        TextView satay = findViewById(R.id.integer_number_3);
+        if(Integer.parseInt(satay.getText().toString())>0){
         count_3 = count_3 - 1;
         total_cost=total_cost-pricing_satay;//writing the price of the food
         display_4(total_cost);
 
-        display_3(count_3);
+        display_3(count_3);}else{//preventing the counter for satay to go down below 0
+            display_3(0);
+        }
     }
 
     private void display_3(int number) {
@@ -109,7 +128,7 @@ public class CashierActivity extends AppCompatActivity {
                 R.id.integer_number_3);
         displayInteger.setText("" + number);
     }
-    private void display_4(double number){
+    private void display_4(double number){//display for the total cost value
         TextView displayInteger = findViewById(R.id.totalcostvalue);
         displayInteger.setText(""+number);
     }
--- app/src/main/java/com/example/android1/StatsActivity.java
+++ app/src/main/java/com/example/android1/StatsActivity.java
@@ -2,6 +2,7 @@ package com.example.android1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class StatsActivity extends AppCompatActivity {
@@ -10,10 +11,11 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        Bundle foodstats =getIntent().getExtras();//this will collect any intent data that is passed into this activity
-        if(foodstats==null){
-            return;
-        }
+        Intent intent = getIntent();//this will collect any intent data that is passed into this activity
+        Double chickenricefee = intent.getDoubleExtra("Chickenrice",0);
+        Double satay=intent.getDoubleExtra("satayricenumber",0);
+        Double duckricenumber =intent.getDoubleExtra("duckricenumber",0);
+
         // so from here i have gotten the key values pairs , nameley the satay chickenrice duckrice keys and their values like how many food
 
 
