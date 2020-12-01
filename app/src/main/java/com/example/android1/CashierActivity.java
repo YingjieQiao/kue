30186e410e6891d999f00f020a54fdd0b15b8709
chuaqibao <chuaqibao@gmail.com>
2020-12-01 15:15:58 +0800

    test commit

--- app/src/main/java/com/example/android1/CashierActivity.java
+++ app/src/main/java/com/example/android1/CashierActivity.java
@@ -30,6 +30,8 @@ public class CashierActivity extends AppCompatActivity {
     DatabaseReference foodMenu;
     DatabaseReference order;
     DatabaseReference webOrder;
+    DatabaseReference stats;
+    DatabaseReference foodStats;
     Double totalCost;
     HashMap<String, String> orderLs = new HashMap<>();
 
@@ -38,56 +40,49 @@ public class CashierActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cashier);
 
+        // text views and btn
         cost = findViewById(R.id.totalCost);
         orderList = findViewById(R.id.orderList);
         submitBtn = findViewById(R.id.submitbutton);
 
+        // firebase references
         order = FirebaseDatabase.getInstance().getReference().child("accounts").child("username1").child("orders");
         webOrder = FirebaseDatabase.getInstance().getReference().child("accounts").child("username1").child("order_web");
         foodMenu = FirebaseDatabase.getInstance().getReference().child("accounts").child("username1").child("foods");
+        stats = FirebaseDatabase.getInstance().getReference().child("accounts").child("username1").child("stats");
+        foodStats = FirebaseDatabase.getInstance().getReference().child("accounts").child("username1").child("stats").child("food");
 
-        retrieveFoodMenu();
+
+        retrieveFoodMenu(); // query all food info from firebase
 
         submitBtn.setOnClickListener(v -> {
-
-            String receiptId = UUID.randomUUID().toString();//this will generate a random uuid for the receipt order
-            orderLs.put("totalCost", Double.toString(totalCost));
-            orderLs.put("receiptOrder", receiptId);
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
-            Date orderDate = new Date();
-            Order newOrder = new Order(orderLs, System.currentTimeMillis(), (long) -1, dateFormat.format(orderDate), receiptId);
-            order.push().setValue(newOrder);
-            webOrder.push().setValue(newOrder);
-
-            Toast.makeText(CashierActivity.this,"Your order" + " has been submitted", Toast.LENGTH_SHORT).show();
-
-            // reset order
-            totalCost = 0.;
-            orderLs.clear();
-            cost.setText("0");
-            orderList.setText("No Order");
+            statsUpdate(); // update statistics
+            submitOrder(); // for cashier to submit an order
         });
 
     }
 
+    // query all food info (list of foods, prices, eta) from firebase
     private void retrieveFoodMenu() {
         foodMenu.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
+                // create ArrayList for all foods, prices and their ETAs
                 ArrayList<String> foodList = new ArrayList<>();
                 ArrayList<Double> foodCost = new ArrayList<>();
                 ArrayList<String> foodETA = new ArrayList<>();
 
                 if (snapshot.hasChildren()) {
                     for (DataSnapshot myDataSnapshot : snapshot.getChildren()) {
-                        HashMap<String, Object> foodInfo = (HashMap<String, Object>) myDataSnapshot.getValue();
+                        HashMap<String, Object> foodInfo = (HashMap<String, Object>) myDataSnapshot.getValue(); // get "food" data from firebase
 
-                        foodList.add(myDataSnapshot.getKey());
-                        foodCost.add((Double) foodInfo.get("price"));
-                        foodETA.add(foodInfo.get("eta").toString());
+                        foodList.add(myDataSnapshot.getKey()); // add to the ArrayList of all foods
+                        foodCost.add((Double) foodInfo.get("price")); // add to the ArrayList of all prices
+                        foodETA.add(foodInfo.get("eta").toString()); // add to the ArrayList of the ETA for each food
 
                     }
+                    // create buttons programmatically
                     createMenuButtons(foodList, foodCost, foodETA);
                 }
             }
@@ -98,19 +93,26 @@ public class CashierActivity extends AppCompatActivity {
 
     }
 
+    // create buttons programmatically for all foods to make orders
     private void createMenuButtons(ArrayList<String> foodList, ArrayList<Double> foodCost, ArrayList<String> foodETA) {
 
         LinearLayout layout = findViewById(R.id.CashierActivity);
         totalCost = 0.;
 
+        //loop through the food ArrayList to create the add/delete food buttons
         for (String food: foodList) {
-            Button addFoodBtn = new Button(this);
-            addFoodBtn.setText("Add " + food);
-            addFoodBtn.setId(foodList.indexOf(food)+10); // index + 10 is the id for the button to add food
-            layout.addView(addFoodBtn);
 
+            //create the "Add" & "Delete" buttons for each food to be added to the menu
+            Button addFoodBtn = new Button(this);
+            Button removeFoodBtn = new Button((this));
+            addFoodBtn.setText("Add " + food);
+            removeFoodBtn.setText("Delete " + food);
+            layout.addView(addFoodBtn);
+            layout.addView(removeFoodBtn);
+
+            //add a food to the order
             addFoodBtn.setOnClickListener(v -> {
-                totalCost += foodCost.get(foodList.indexOf(food));
+                //totalCost += foodCost.get(foodList.indexOf(food)); // calculate total cost of food
 
                 if (orderLs.containsKey(food)) {
                     int foodCnt = Integer.parseInt(orderLs.get(food));
@@ -121,22 +123,60 @@ public class CashierActivity extends AppCompatActivity {
                 cost.setText(String.valueOf(totalCost));
             });
 
-            Button removeFoodBtn = new Button((this));
-            removeFoodBtn.setText("Delete " + food);
-            removeFoodBtn.setId((foodList.indexOf(food))); //index is the id for the button to remove food
-            layout.addView(removeFoodBtn);
-
+            //remove selected food from order
             removeFoodBtn.setOnClickListener(v -> {
                 if (orderLs.containsKey(food)) {
                     int foodCnt = Integer.parseInt(orderLs.get(food));
                     if (foodCnt > 0) {
-                        orderLs.put(food, Integer.toString(foodCnt - 1));
-                        totalCost -= foodCost.get(foodList.indexOf(food));
+                        orderLs.put(food, Integer.toString(foodCnt - 1)); //remove 1 from the food order count
+                        //totalCost -= foodCost.get(foodList.indexOf(food)); // calculate total cost of food
                     }
                 }
                 orderList.setText(orderLs.toString());
                 cost.setText(String.valueOf(totalCost));
             });
         }
+    }
+
+    // submit a new order
+    private void submitOrder() {
+
+        // adding order details
+        String receiptId = UUID.randomUUID().toString();//this will generate a random uuid for the receipt order
+        orderLs.put("totalCost", Double.toString(totalCost));
+        orderLs.put("receiptOrder", receiptId);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+        Date orderDate = new Date();
+
+        // create the order
+        Order newOrder = new Order(orderLs, System.currentTimeMillis(), (long) -1, dateFormat.format(orderDate), receiptId);
+
+        // send order to firebase
+        order.push().setValue(newOrder);
+        webOrder.push().setValue(newOrder);
+
+        // inform that the order has been made
+        Toast.makeText(CashierActivity.this,"Your order" + " has been submitted", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    private void statsUpdate() {
+
+        stats.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Stats statistics = snapshot.getValue(Stats.class);
+                //System.out.println("Stats: " + snapshot.getValue());
+                //System.out.println("Stats Class: " + snapshot.getValue().getClass());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/android1/Stats.java
@@ -0,0 +1,18 @@
+package com.example.android1;
+
+import java.util.HashMap;
+
+public class Stats {
+
+    HashMap<String, Long> customerTraffic;
+    HashMap<String, Long> food;
+    Double averageRating;
+    Double dailyRevenue;
+    Long totalRatings;
+
+    public Stats(Long orderTime, HashMap<String, Long> food, Long averageRating, Long dailyRevenue, Long totalRatings) {
+        this.customerTraffic = customerTraffic;
+
+    }
+
+}
