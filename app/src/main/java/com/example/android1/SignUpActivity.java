083056f822f5decf15c15801aa91e2e18ba907de
chuaqibao <chuaqibao@gmail.com>
2020-12-01 21:20:49 +0800

    minor ui updates

--- app/src/main/java/com/example/android1/SignUpActivity.java
+++ app/src/main/java/com/example/android1/SignUpActivity.java
@@ -23,42 +23,46 @@ import java.util.HashSet;
 import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
+
     EditText email_edit;
     EditText password_edit;
     EditText password_verify_edit;
     Button sign_up_edit;
     DatabaseReference accountRef = FirebaseDatabase.getInstance().getReference().child("accounts");
     static final String TAG = "SignUpPage";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
         email_edit = findViewById(R.id.signup_username);
         password_edit = findViewById(R.id.signup_password);
         password_verify_edit = findViewById(R.id.signup_password_verify);
         sign_up_edit = findViewById(R.id.button_finish_signup);
 
-        sign_up_edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String password = password_edit.getText().toString();
-                String password_verify = password_verify_edit.getText().toString();
-                String email = email_edit.getText().toString();
-                String username = email.split("@")[0];
-                Log.i(TAG, password);
-                Log.i(TAG, password_verify);
-                if (!password.equals(password_verify)){
-                    Toast.makeText(SignUpActivity.this, "passwords are not the same!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                HashMap<String, Object> dataToBePushed = new HashMap<>();
-                dataToBePushed.put("email", email);
-                dataToBePushed.put("password", password);
-                accountRef.child(username).setValue(dataToBePushed);
-                Toast.makeText(SignUpActivity.this, " Sign up Suceefully", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                startActivity(intent);
+        sign_up_edit.setOnClickListener(v -> {
+            String password = password_edit.getText().toString();
+            String password_verify = password_verify_edit.getText().toString();
+            String email = email_edit.getText().toString();
+            String username = email.split("@")[0];
+            Log.i(TAG, password);
+            Log.i(TAG, password_verify);
+
+            if (!password.equals(password_verify)){
+                Toast.makeText(SignUpActivity.this, "passwords are not the same!", Toast.LENGTH_LONG).show();
+                return;
             }
+
+            HashMap<String, Object> dataToBePushed = new HashMap<>();
+
+            dataToBePushed.put("email", email);
+            dataToBePushed.put("password", password);
+            accountRef.child(username).setValue(dataToBePushed);
+
+            Toast.makeText(SignUpActivity.this, " You have created a new account", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
